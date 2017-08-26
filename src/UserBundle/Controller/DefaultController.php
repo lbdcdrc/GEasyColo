93ab33875e6f002aa46a9f7f71255fd7a882dde3
lbdcdrc <c.lombard@perisco.telligo.fr>
2017-08-26 12:09:51 +0200

    Factorize code

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -17,39 +17,27 @@ class DefaultController extends Controller
 {
     public function photoAction(Request $request)
     {
-		$user = $this->getUser();
+		$Image=$this->getUser()->getImage();
 		
-		if ($user->getImage()===null)
+		if ($this->getUser()->getImage()===null)
 		{
 			$Image=new Image();
 		}
-		else
-		{
-			$Image=$user->getImage();
-		}
-		
-		if (null === $user)
-		{
-			throw new NotFoundHttpException("Aucun utilisateur n'est connecté...");
-		}
-		
 		
 		$form = $this->get('form.factory')->create(ImageType::class, $Image);
-
 
 		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) 
 		{
 			$user->setImage($Image);
 			$this->getDoctrine()->getManager()->flush();
 			$request->getSession()->getFlashBag()->add('notice', 'L\'avatar a bien été modifié');
-			$URLImage = $user->getImage();
 
-				$cacheManager = $this->get('liip_imagine.cache.manager');
-				$cacheManager->remove($URLImage->getwebPath(), 'my_thumb');
-				$cacheManager->remove($URLImage->getwebPath(), 'md_thumb');
-				$cacheManager->remove($URLImage->getwebPath(), 'lg_thumb');
+			$cacheManager = $this->get('liip_imagine.cache.manager');
+			$cacheManager->remove($user->getImage()->getwebPath(), 'my_thumb');
+			$cacheManager->remove($user->getImage()->getwebPath(), 'md_thumb');
+			$cacheManager->remove($user->getImage()->getwebPath(), 'lg_thumb');
 			
-		  return $this->redirectToRoute('fos_user_profile_show');
+			return $this->redirectToRoute('fos_user_profile_show');
 		}		
 
 		return $this->render('UserBundle:Default:editpicture.html.twig', array('form' => $form->createView(),));
